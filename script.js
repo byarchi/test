59f21fe8b9e046bf911a050c6c82038903ab32a6
Archi <artur4330@gmail.com>
2019-04-09 12:10:49 +0300

    Это мой первый комит

---
+++ script.js
@@ -0,0 +1,17 @@
+var leftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+let persone = {
+    name: 'John',
+    age: 25,
+    isMarried: false
+};
+
+console.log(persone['name']);
+
+let arr = ['plum.png', 'ornge.jpg', 'apple.bmp'];
+
+console.log(arr[2]);
+
+let answer = prompt('Есть ли вам 18', 'Да');
+console.log(typeof(persone));
